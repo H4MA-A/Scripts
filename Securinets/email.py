9c07cefdc69471ecb7b7b6e2883e2d5d281e57b4
Mohamed Arfaoui <mohamed.arfaoui@insat.u-carthage.tn>
2021-01-02 04:35:26 +0100

    Update email.py

--- Securinets/email.py
+++ Securinets/email.py
@@ -12,12 +12,12 @@ j=0;
 count_success=0
 for i in name_list:
     try:
-        sg = sendgrid.SendGridAPIClient(api_key="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")
+        sg = sendgrid.SendGridAPIClient(api_key="#####REDACTED#####")
         from_email = Email("contact@securinets.com")
         to_email = To(mail_list[j])
         mail = Mail(from_email, to_email)
         mail.dynamic_template_data = {'Name' : i,'ID' : ID_list[j], 'Email' : mail_list[j]}
-        mail.template_id = "d-05489dd6e7d344aeb28ad6d1cbf7610c"
+        mail.template_id = "#####REDACTED#####"
         response = sg.client.mail.send.post(request_body=mail.get())
         print(response.status_code)
         print(response.body)
@@ -34,5 +34,3 @@ for i in name_list:
         print(mail_list[j])
         j+=1
         continue
-#d-5d6869def0534aeeb1f169a05c45c6a6
-#api_key: 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
